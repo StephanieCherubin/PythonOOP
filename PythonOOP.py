4ef716025bea0659b5d0c8e8ce04740e720b2f96
Stephanie Cherubin <seraphin.cherubin@gmail.com>
2018-08-27 07:28:30 -0700

    create subclasses

--- PythonOOP.py
+++ PythonOOP.py
@@ -1,41 +1,32 @@
 class Employee:
 
-    num_of_emp = 0
     raise_amount = 1.04
 
     def __init__(self, first, last, pay):
         self.first = first
         self.last = last
-        self.email = first + '@' + last + '.com'
+        self.email = first + '.' + last + '@email.com'
         self.pay = pay
-
-        Employee.num_of_emp += 1
 
     def fullname(self):
         return '{} {}'.format(self.first, self.last)
 
     def apply_raise(self):
-        self.pay = int(self.pay * Employee.raise_amount)
+        self.pay = int(self.pay * self.raise_amount)
 
-    @classmethod
-    def set_raise_amount(cls, amount):
-        cls.raise_amount = amount
+class Developer(Employee):
+    raise_amount = 1.10
 
-    @classmethod
-    def from_string(cls, emp_string):
-        first, last, pay = emp_string.split('-')
-        return cls(first, last, pay)
+    def __init__(self, first, last, pay, prog_lang):
+        super().__init__(first, last, pay)
+        self.prog_lang = prog_lang
 
-    @staticmethod
-    def is_workday(day):
-        if day.weekday() == 5 or day.weekday() == 6:
-            return False
-        return True
+dev_1 = Developer('Terry', 'Parsons', 50000, 'Python')
+dev_2 = Developer('Taylor', 'Law', 70000, 'Javascript')
 
-emp_1 = Employee('Shiela', 'Pierre', 80000)
-emp_2 = Employee('Taylor', 'Law', 70000)
+print(dev_1.email)
+print(dev_1.prog_lang)
 
-import datetime
-my_date = datetime.date(2108, 8, 30)
-
-print(Employee.is_workday(my_date))
+# print(dev_1.pay)
+# dev_1.apply_raise()
+# print(dev_1.pay)
